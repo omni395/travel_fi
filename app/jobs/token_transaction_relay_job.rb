3b35bf3419b4643e4a6a3aedca175c22594efc9e
Dimon <omni395@gmail.com>
2026-08-28 13:24:16 +0300

    fix: update puma to fix CVE vulnerabilities

--- app/jobs/token_transaction_relay_job.rb
+++ app/jobs/token_transaction_relay_job.rb
@@ -36,14 +36,14 @@ class TokenTransactionRelayJob < ApplicationJob
 
     # БД-блокировка: только одна Job за раз может войти в этот блок
     # (гарантирует последовательный nonce без конфликтов в мемпуле)
-    sql_lock = ActiveRecord::Base.sanitize_sql_array(["SELECT pg_advisory_lock(?)", lock_id])
+    sql_lock = ActiveRecord::Base.sanitize_sql_array([ "SELECT pg_advisory_lock(?)", lock_id ])
     ActiveRecord::Base.connection.execute(sql_lock)
 
     begin
       Rails.logger.info("TokenTransactionRelayJob: acquired lock (tx_id=#{token_transaction_id})")
       TokenTransactionService.relay!(transaction)
     ensure
-      sql_unlock = ActiveRecord::Base.sanitize_sql_array(["SELECT pg_advisory_unlock(?)", lock_id])
+      sql_unlock = ActiveRecord::Base.sanitize_sql_array([ "SELECT pg_advisory_unlock(?)", lock_id ])
       ActiveRecord::Base.connection.execute(sql_unlock)
       Rails.logger.info("TokenTransactionRelayJob: released lock (tx_id=#{token_transaction_id})")
     end
